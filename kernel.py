f894061199a01b8378d14e7f232969850f0bf8ad
ZiyadOulhaj <oulhaj.ziyad@gmail.com>
2022-02-10 06:44:28 +0100

    spectral clustering update

--- kernel.py
+++ kernel.py
@@ -1,8 +1,9 @@
-# -*- coding: utf-8 -*-
+# %load "C:\Users\Ziyad\Desktop\Kernel_methods\kernel.py"
 from math import exp, sqrt
 import numpy.linalg as npl
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
 def Gk_image(x,t,sigma=1):
     """
@@ -87,31 +88,43 @@ class Gk_PCA:
         self.K=Gk_gram_matrix(self.dataset,self.sigma)
         
     def compute_directions(self):
+        
+        ## Centered Gram matrix
         n=self.K.shape[0]
-        w,v=npl.eig(self.K)
+        m=np.ones((n,n))/n
+        i=np.eye(n)
+        self.K_c=(i-m) @ self.K @ (i-m)
+        
+        ## Eigenvalues
+        w,v=npl.eigh(self.K_c)
         order=np.argsort(-w)
         w=w[order]
         v=v[:,order]
+        i=0
+        while w[i]>=0:
+            i=i+1
+        w=w[0:i]
+        v=v[:,0:i]
         
         #Directions
-        D=np.zeros((n,n))
-        for i in range (n):
-            D[:,i]=v[:,i]/sqrt(w[i])
+        D=np.zeros((n,i))
+        for j in range (i):
+            D[:,j]=v[:,j]/sqrt(w[j])
         
         self.directions=D
         
         #Explained_variance
-        for i in range (n):
-            self.explained_variance.append(1/n*D[:,i].T
-                @ self.K @ self.K @ D[:,i])
+        for j in range (i):
+            self.explained_variance.append(1/n*D[:,j].T
+                @ self.K_c @ self.K_c @ D[:,j])
         
         
         
     def reduce(self):
-        return self.K @ self.directions
+        return self.K_c @ self.directions
     
     def scree_plot(self):
-        x = np.arange(self.K.shape[0]) + 1
+        x = np.arange(len(self.explained_variance))+1
         plt.plot(x, self.explained_variance, 'o-', linewidth=2, color='blue')
         plt.title('Scree Plot')
         plt.xlabel('Principal Component')
@@ -124,3 +137,40 @@ class Gk_PCA:
         plt.xlabel('First component')
         plt.ylabel('Second component')
         plt.show()
+                
+            
+#--------------------------------------------------------
+
+class Gk_SpectralClustering :
+    
+    def __init__(self,nb_clusters,dataset,sigma=1):
+        self.k=nb_clusters
+        self.dataset=dataset
+        self.sigma=sigma
+        
+    def compute_gram_matrix(self):
+        self.K=Gk_gram_matrix(self.dataset,self.sigma)
+        
+    def compute_clusters(self):
+        ## Centered Gram matrix
+        n=self.K.shape[0]
+        m=np.ones((n,n))/n
+        i=np.eye(n)
+        self.K_c=(i-m) @ self.K @ (i-m)
+        
+        ## Eigenvalues
+        w,v=npl.eigh(self.K_c)
+        order=np.argsort(-w)
+        w=w[order]
+        v=v[:,order]
+        self.Z=v[:,0:self.k]
+        
+        ## Normalize Z
+        A=self.Z
+        norm=npl.norm(A,axis=1)
+        for i in range(n):
+            A[i,:]=A[i,:]/norm[i]
+        
+        ## Regular KMeans to A
+        km=KMeans(n_clusters=self.k)
+        return(km.fit_predict(A))
